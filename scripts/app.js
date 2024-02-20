bf90461ebe14c46438e6cbc4a05fe504d88c015c
mhmitas <mahfuzulmitas@gmail.com>
2024-02-20 15:48:16 +0600

    commit commit

--- scripts/app.js
+++ scripts/app.js
@@ -7,3 +7,13 @@ function cartProductCount(){
 
 
 // document.getElementById('product-img-container').style.display = 'none'
+const cart = document.getElementById('cart')
+cart.addEventListener('click', function(){
+    const cartDetails = document.getElementById('cart-details')
+    
+    // if(cartDetails.style.display === 'none'){
+    //     cartDetails.style.display = 'block'
+    // }else{
+    //     console.log('hello')
+    // }
+})
