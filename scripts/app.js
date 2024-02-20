97c5cef46ce2b3f978cd572f2fac092e252b3f4f
mhmitas <mahfuzulmitas@gmail.com>
2024-02-20 17:55:35 +0600

    commit commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,47 @@
-function cartProductCount(){
-    const currentProductNum = getElementValueById('product-quantity')
-    const updatedProductNum = currentProductNum + 1;
-    // console.log(updatedProductNum)
-    setElementValuebyId('product-quantity',updatedProductNum)
+
+
+
+
+// cartDetails-------------------------
+function cartDetails() {
+    const cartDetails = document.getElementById('cart-details')
+    if (cartDetails.style.display === 'block') {
+        cartDetails.style.display = 'none'  // dekha jabe na 
+    } else {
+        cartDetails.style.display = 'block' // block mane dekha hobe.
+    }
 }
 
 
-// document.getElementById('product-img-container').style.display = 'none'
-const cart = document.getElementById('cart')
-cart.addEventListener('click', function(){
-    const cartDetails = document.getElementById('cart-details')
-    
-    // if(cartDetails.style.display === 'none'){
-    //     cartDetails.style.display = 'block'
-    // }else{
-    //     console.log('hello')
-    // }
-})
+// update number in cart box
+function cartProductCount() {
+    const currentProductNum = getInnerInt('productCount')
+    const updatedProductNum = currentProductNum + 1;
+    setElementValuebyId('productCount', updatedProductNum)
+}
+
+
+
+
+
+
+
+
+
+// --------------------
+// control + - button
+const productQuantity = getInnerInt('product-quantity')
+console.log(productQuantity)
+
+function byPlusMinusBtn() {
+    plusBtn = document.getElementById('plus-btn')
+    minusBtn = document.getElementById('minus-btn')
+}
+
+
+
+function getInnerInt(id){
+    const element = document.getElementById(id).innerText;
+    const int = parseFloat(element)
+    return int;
+}
